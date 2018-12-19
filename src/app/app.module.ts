788592bffa1b69535546ba662668242161e31986
Lily Romero <lilyromero.lopez@gmail.com>
2018-12-19 13:34:56 -0600

    Menu izquierdo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { VMenuComponent } from './v-menu/v-menu.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    VMenuComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
